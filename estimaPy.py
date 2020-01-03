87cb15b644a6466414e956f4adeb3830479b4cac
Roberto Hernando <roberto@rhernando.net>
2020-01-03 13:09:17 +0100

    commit inicial

---
+++ estimaPy.py
@@ -0,0 +1,10 @@
+# Estimación de pi mediante el método de Montecarlo
+
+# 1. Se define el número de intentos
+INTENTOS = 100
+
+# 2. Se hace la tirada
+
+# 3. Se ve si está dentro del círculo
+
+# 4. Se estima pi como aciertos/intentos
